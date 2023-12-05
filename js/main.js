0b75f70764782a853bec3d55093f072ba99610d9
Neha28sharma1 <148784962+Neha28sharma1@users.noreply.github.com>
2023-12-05 16:37:19 +0100

    Add to cart function added

---
+++ js/main.js
@@ -0,0 +1,20 @@
+let num = 1;
+const buttonElement = document.querySelectorAll(".cart-button");
+const countIcon = document.querySelector(".no-of-items");
+const displayNumber = document.querySelector(".display-count");
+
+//buttonElement.forEach((cart-button) => {buttonElement.addEventListener("click", function (event) {
+
+//displayNumber.innerText = num++;
+//displayNumber.style.color = "#ffffff";
+//});
+//});
+
+buttonElement.forEach(clickButton);
+function clickButton(addToCart) {
+  addToCart.addEventListener("click", function (event) {
+    countIcon.classList.remove("hide");
+    displayNumber.innerText = num++;
+    displayNumber.style.color = "#ffffff";
+  });
+}
